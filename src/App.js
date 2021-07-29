8885c42e25d75f3dae6c6c0eba92ebb43dcc0fd4
Daniel Lafontant <ldanieljajr@gmail.com>
2021-07-29 12:38:25 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 
-//component
+//pages
 import Home from './pages/Home'
 import About from './pages/About'
 import ArticleList from './pages/ArticleList';
 import Article from './pages/Article';
 
+//component
+import NavBar from './components/NavBar';
+
 
 function App() {
   return (
     <Router>
+      <NavBar />
     <div className="max-w-screen-md mx-auto pt-20">
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function NavBar() {
+    return (
+        // w-full to create the with of the navbar
+        <nav className="border-b-4 border-gree-700 text-center fixed top-0  w-full bg-green-600 font-bold text-lg text-white">
+            <ul>
+                <li className="inline-block pt-4 pb-4">
+                    <Link to="/" className="pl-6 pr-8">Home</Link>
+                </li>
+
+                <li className="inline-block pt-4 pb-4">
+                    <Link to="/about" className="pl-6 pr-8">About</Link>
+                </li>
+
+                <li className="inline-block pt-4 pb-4">
+                    <Link to="/articles-list" className="pl-6 pr-8">Article List</Link>
+                </li>
+
+                <li className="inline-block pt-4 pb-4">
+                    <Link to="/article" className="pl-6 pr-8">Article</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
