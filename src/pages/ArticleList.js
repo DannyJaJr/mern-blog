6246c503559723a0a56163dcbdc2d9ec71a09cc6
Daniel Lafontant <ldanieljajr@gmail.com>
2021-07-29 16:38:45 -0400

    Article coponents created

--- src/pages/ArticleList.js
+++ src/pages/ArticleList.js
@@ -1,44 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+
 import articleContent from './article-content'
+import Article from '../components/Articles'
 
 function ArticleList() {
     return (
         <>
             <h1 className="sm:text-4xl text-2xl font-bold mb-6 mt-6 text-gray-900">
-                Articles list page
+                Articles list page :
             </h1>
             <div className="container py-4 mx-auto">
                 <div className="flex flex-wrap m-4">
-                    {articleContent.map((article, index)=> (
-                        <div key={index} className="p-4 md:w-1/2">
-                            <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                                <Link to={`/article/${article.name}`}>
-                                    <img  className="lg:h-48 md:h-36 w-full object-cover object-center" 
-                                    src={article.thumbnail} alt="blog"/>
-                                </Link>
-                                <div className="p-6">
-                                    <Link key={index} to={`/article/${article.name}`}>
-                                        <h3 className="title-font text-lg font-medium text-gary-900 mb-3">{article.title}</h3>
-                                    </Link>
-                                    <p className="leading-relaxed mb-3">
-                                        {article.content[0].substring(0, 120)}...
-                                    </p>
-
-                                    <div className="flex item-center flex-wrap">
-                                        <Link className="text-red-500 inline-flex items-center md:mb-2 lg:mb-0"
-                                        to={`/article/${article.name}`}>
-                                            Read me 
-                                        </Link>
-
-                                    </div>
-
-                            
-                                </div>
-                            </div>
-
-                        </div>
-                    ))}
+                    <Article articles={articleContent} />
+                   
 
                 </div>
 
