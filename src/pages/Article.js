e8841fce0800fe18ddaee9cf7827a304a4005af8
Daniel Lafontant <ldanieljajr@gmail.com>
2021-07-29 13:10:10 -0400

    artile-content.js

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import articleContent from './article-content'
 
-function Article() {
+function Article({match}) {
+    const name = match.params.name;
+    const article = articleContent.find((article) => article.name === name);
+    if (!article) return <h1>Article does not exists</h1>
+   
     return (
-        <div>
-            <h1 className="sm:text-4xl text-2xl font-bold mb-6 mt-6 text-gray-900">This is an article</h1>
+        <>
+             <h1 className='sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900'>
+        {article.title}
+      </h1>
+      {article.content.map((paragraph, index) => (
+        <p className='mx-auto leading-relaxed text-base mb-4' key={index}>
+          {paragraph}
+        </p>
+      ))}
             
-        </div>
+        </>
     )
 }
 
-export default Article
+export default Article;
