7d131c237ed44b66d2198e1f14177e40cac5e394
Daniel Lafontant <ldanieljajr@gmail.com>
2021-07-29 21:34:22 -0400

    nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function NavBar() {
     return (
         // w-full to create the with of the navbar
-        <nav className="border-b-4 border-gree-700 text-center fixed top-0  w-full bg-green-600 font-bold text-lg text-white">
+        <nav className="border-b-4 border-blue-700 text-center fixed top-0  w-full bg-green-600 font-bold text-lg text-white">
             <ul>
                 <li className="inline-block pt-4 pb-4">
                     <Link to="/" className="pl-6 pr-8">Home</Link>
